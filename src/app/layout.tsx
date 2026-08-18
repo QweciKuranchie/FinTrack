6c3c8683f5346dce7e9f10ad64b4f533e8aee59c
QweciKuranchie <nuhurichard47@gmail.com>
2026-08-18 23:50:19 +0000

    [PWA] Add icon-192.png and icon-512.png manifest assets and mobile-web-app-capable metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,9 @@ export const metadata: Metadata = {
     statusBarStyle: "default",
     title: "FinTrack",
   },
+  other: {
+    "mobile-web-app-capable": "yes",
+  },
 };
 
 export const viewport: Viewport = {
